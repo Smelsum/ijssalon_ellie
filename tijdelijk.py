80923cf7be3616d2ebc5615beda9fb33c691edac
Melchior Mak <melchiorvmak@gmail.com>
2024-04-24 15:49:17 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,16 @@
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+aanbieding = 3 * 0.8
+reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}.")
+reclame_tekst2 = reclame_tekst[:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split(" ")
+el = reclame_tekst4
+for i in el: 
+    if len >= 5:
+        print(reclame_tekst4.upper())
+    if len < 5:
+        print(reclame_tekst4.lower())
